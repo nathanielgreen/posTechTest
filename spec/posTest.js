7be6b5ba2fcc6bc846b7ea2b481767c96c5e7f08
nathanielgreen <ngreen2303@gmail.com>
2015-11-24 18:19:50 +0000

    Added twoGetThirdFree function and test. All tests passing.

--- spec/posTest.js
+++ spec/posTest.js
@@ -5,13 +5,19 @@ describe('pos', function(){
   });
 
   it("can return correct price totals", function(){
-    pos.checkout(['B', 'A', 'B', 'P', 'B'], {'A':25, 'B':40, 'P':30})
-    expect(pos.totalPrice).toEqual(155);
+    pos.checkout(['A', 'B', 'P'], {'A':25, 'B':40, 'P':30})
+    expect(pos.totalPrice).toEqual(95);
   });
   
   it("can apply the offer of 'Three for One Pound'", function(){
-    pos.checkout(['B', 'B', 'B'], {'A':25, 'B':40, 'P':30}) 
-    expect(pos.totalPrice).toEqual(100);
+    pos.checkout(['B', 'A', 'B', 'P', 'B'], {'A':25, 'B':40, 'P':30}) 
+    expect(pos.totalPrice).toEqual(155);
+  });
+
+  it("can apply the offer of 'Buy Two, get a Third Free'", function(){
+    pos.checkout(["A", "A", "B"], {'A':25, 'B':40, 'P':30})
+    console.log(pos.finalOrder);
+    expect(pos.finalOrder).toEqual(["A', 'A', 'B", "A" ]);
   });
 
 });
--- src/pos.js
+++ src/pos.js
@@ -1,25 +1,27 @@
 function POS(){
   this.totalPrice = 0;
   this.priceArr = new Array();
+  this.finalOrder = []
 };
 
-POS.prototype.checkout = function(order, pricing) {
+POS.prototype.checkout = function(order, pricing){
   if (this.threeForOne(order)){
-    for (i = 0; i < order.length; i++){
-      this.priceArr.push(pricing[order[i]]);
-    };
+    this.priceLoop(order, pricing);
     this.totalPrice -= 20;
+  }
+  else if (this.twoGetThirdFree) {
+    this.priceLoop(order, pricing);  
+    this.finalOrder.push('A');
   } 
   else {
-    for (i = 0; i < order.length; i++){
-      this.priceArr.push(pricing[order[i]]);
-    };
+    this.priceLoop(order, pricing);
   };
 
   for (i = 0; i < this.priceArr.length; i++){
     this.totalPrice += parseInt(this.priceArr[i]);
   };
 };
+
 
 POS.prototype.threeForOne = function(order){
   var counter = 0;
@@ -34,11 +36,33 @@ POS.prototype.threeForOne = function(order){
 };
 
 
-function Checkout(){
-
+POS.prototype.twoGetThirdFree = function(){
+  var counter = 0;
+  for (var i = 0; i < order.length; i++) {
+    if (order[i] === 'A'){
+      counter++
+    }
+  };
+  if (counter == 2) {
+   return true
+  }; 
 };
 
-Checkout.prototype.scan = function(itemCode){
 
+POS.prototype.priceLoop = function(order, pricing){
+  this.finalOrder.push(order.join("', '"));
+  for (i = 0; i < order.length; i++){
+    this.priceArr.push(pricing[order[i]]);
+  };
 };
+
+
+
+// function Checkout(){
+// 
+// };
+// 
+// Checkout.prototype.scan = function(itemCode){
+// 
+// };
 
